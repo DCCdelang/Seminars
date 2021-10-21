29925ca015b0c051eed3fa8b9982356694dddc16
FloTig <floriantiggeloven@hotmail.com>
2021-10-21 16:15:29 +0200

    2a

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,11 +9,13 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+var_1 = function_2c(1000,100,100,-50)['add']
 
-var_2 = function_2c(...)
+var_2 = function_2b('Seminars','Borrel')['C']
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str(function_2c(100,1000,10,100)['multiply']) + function_2b('cLs','poop')['L']
+
+
 
 if var_1 == 950:
     print("Good job!")
