34979d88598e22742edd72edf5394b9c19bf3348
jarih <jari.hoffmann@gmail.com>
2021-10-21 16:11:31 +0200

    added documentation to 2b and 2c

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,17 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    """
+    Argument:
+    string1 -- text you want to turn into lower case
+    string2 -- text you want to turn into upper case
 
+    Returns:
+    dict -- dictionary with:
+            1. lower case version of string1
+            2. upper case version of string2
+            3. combination of string1 and string2
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
--- assignment_2c.py
+++ assignment_2c.py
@@ -5,6 +5,17 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """
+    Argument:
+    w, x, y, z -- numbers for calculations
+
+    Returns:
+    results -- dictionary with:
+            1. x multiplied with y
+            2. x divided by y
+            3. w and z added together
+            4. w minus z
+    """
 
     multiplication = x * y
     division = x / y
