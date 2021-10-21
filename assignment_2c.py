dbd8f27718a578b7d3bfba3c2e12cbf945b741c5
Thijs Blom <matthijs.b.blom@gmail.com>
2021-10-21 16:24:00 +0200

    Add documentation to 2b and 2c

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,17 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """Perform some basic arithmetic
+    Arguments:
+        w,x,y,z     Numbers to perform calculations on
 
+    Returns:
+        Dictionary containing the following keys:
+        multiply    x * y
+        divide      x / y
+        add         w + z
+        subtract    w - z
+        """
     multiplication = x * y
     division = x / y
     addition = w + z
