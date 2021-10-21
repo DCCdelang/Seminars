8227c855c5a870bf09fb857f3360a06edd42fbe7
tstoof <47404105+tstoof@users.noreply.github.com>
2021-10-21 15:56:30 +0200

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -16,7 +16,7 @@ def lower_case(string):
     """
 
     ### your code starts here
-
+    lower_string = string
     ### your code ends here
 
     return lower_string.lower()
@@ -38,7 +38,7 @@ def upper_case(string):
     """
 
     ### your code starts here
-
+    lower_string = string
     ### your code ends here
 
     return lower_string.upper()
