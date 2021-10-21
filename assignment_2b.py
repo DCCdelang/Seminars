c39303d4924fb0ea465914cb2eeab72cc3fb4840
Royenroy <47416991+Royenroy@users.noreply.github.com>
2021-10-21 16:08:08 +0200

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,10 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """
+        Requires to arguments: string 1 and string 2. Returns a dict.
+        "L" gives the lower of string 1, "U" gives the upper of string 2, "C" gives the combination of string 1 and string 2
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
