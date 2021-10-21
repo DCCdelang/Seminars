91193aed545124885174e837c70d4986fbce26a5
Lars1998 <90910928+Lars1998@users.noreply.github.com>
2021-10-21 16:52:44 +0200

    my part

--- assignment_1.py
+++ assignment_1.py
@@ -16,7 +16,7 @@ def lower_case(string):
     """
 
     ### your code starts here
-
+    lower_string = string.lower()
     ### your code ends here
 
     return lower_string
@@ -27,7 +27,7 @@ Do the same thing again with upper_case, but change the order so everyone
 experiences at least 1 merge conflict.
 """
 
-def upper_case(string):
+def upper_case(lower_string):
     """
     Argument:
     string -- text you want to turn into upper case
@@ -38,7 +38,7 @@ def upper_case(string):
     """
 
     ### your code starts here
-
+    string = lower_string.upper()
     ### your code ends here
 
-    return lower_string
+    return string
--- assignment_2c.py
+++ assignment_2c.py
@@ -5,6 +5,20 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """
+    Multiplies x and y, divides x and y, adds up w and z and subtracts w and z 
+    and returns a dict with all of them.
+    
+    Arguments:
+        w -- number which you want to add up and subtract
+        x -- number which you want to multiply and divide
+        y -- number which you want to multiply and divide
+        z -- number which you want to add up and subtract
+        
+    Returns:
+        results -- dictionary of "multiply": x * y, "division": x / y, 
+        "add": w + z and "subtract": w - z
+    """
 
     multiplication = x * y
     division = x / y
