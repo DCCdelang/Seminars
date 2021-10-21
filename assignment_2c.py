35b8bbe071a6f6f3826cce34d697306f8abd5f17
Maloup <89802394+Maloup@users.noreply.github.com>
2021-10-21 16:09:43 +0200

    Documentation for 2c has been added

--- assignment_2c.py
+++ assignment_2c.py
@@ -4,7 +4,16 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+
+
 def function_2c(w, x, y, z):
+
+    """
+    Of the input variables w, x, y, z (integers) it returns a multiplication and division of x and y, and, 
+    a addition and subtraction of w and z, in that order. (x*y), (x/y), (w + z) (w - z)
+     The output is a string. 
+    """
 
     multiplication = x * y
     division = x / y
