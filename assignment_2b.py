84432cc8973e19577dada33a2a9ee7902c76b2f5
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2021-10-21 16:12:25 +0200

    foutje sorry

--- assignment_2b.py
+++ assignment_2b.py
@@ -8,7 +8,7 @@ def function_2b(string_1, string_2):
     """
     The function takes string_1 and string_2 and converts them in respectively lower and upper case. 
     A dictionary will be returned with "L" as key for the lowercase string_1 and "U" for uppercase string_2.
-    A third key is added "C" for the combined strings (lowercase string_1 + uppercase string_2).
+    A third key is added "C" for the combined strings (string_1 + string_2).
     """
     lower = string_1.lower()
     upper = string_2.upper()
