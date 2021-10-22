0c46b72df959548afac74bba173a255f2accc325
filipposdim <83306727+filipposdim@users.noreply.github.com>
2021-10-22 18:33:17 +0200

    2a done

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,11 +9,11 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2b(...)
+var_1 = function_2c(1000, 5 ,10, -50)["add"]
 
-var_2 = function_2c(...)
+var_2 = function_2b("Seminars", "Borrel")["C"]
 
-var_3 = str(function_2b(...)) + function_2c(...)
+var_3 = str(function_2c(-50, 1000, 10, 100)["multiply"]) + str(function_2b("cl", "s")["C"])
 
 if var_1 == 950:
     print("Good job!")
