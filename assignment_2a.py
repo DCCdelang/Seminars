1b3b47ebae7ac09715b0cafd44b28b8a80bf2c31
agolombek <91390904+agolombek@users.noreply.github.com>
2021-10-23 11:08:21 +0200

    Update assignment_2a.py

--- assignment_2a.py
+++ assignment_2a.py
@@ -11,12 +11,15 @@ just give a correct one.
 
 """
 function_2b takes 2 strings as arguments
-it transforms the first string to a lower case string and the second one to an upper case string
-it returns a dictionary with 3 elements, first one is the lowe case string, the second the upper case string
+it transforms the first string to a lower case string and the second one to an 
+upper case string it returns a dictionary with 3 elements, 
+first one is the lowe case string, the second the upper case string
 and the third one is a combination of them.
 """
 
-var_1 = function_2b(...)
+dict1 = function_2b('Seminars', 'Borrel')
+var_2 = dict1['C']
+
 
 '''
 function_2c takes 4 inputs (w, x, y, z), it multiplies x and y, divides x by y, 
@@ -24,15 +27,15 @@ adds w and z, substracts z from w. It stores the results in a dictionary with
 keys multiply, divide, add, substract respectively.
 '''
 
-var_2 = function_2c(...)
+# var_1 = function_2c(...)
 
-var_3 = str(function_2b(...)) + function_2c(...)
+# var_3 = str(function_2b(...)) + function_2c(...)
 
-if var_1 == 950:
-    print("Good job!")
+# if var_1 == 950:
+#     print("Good job!")
 
 if var_2 == "SeminarsBorrel":
     print("Well done!")
 
-if var_3 == "10000cls":
-    print("Excellent!")
+# if var_3 == "10000cls":
+#     print("Excellent!")
