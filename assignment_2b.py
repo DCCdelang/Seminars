f360b68c3345f2262c0eeec12fea2c459c0c6e64
Stephan Bosch <stephanbosch@wcw-stud-145-109-23-207.wireless.uva.nl>
2021-10-27 15:21:35 +0200

    have bever done this

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,16 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    """[You send the function two strings, it takes the first making it all lower case, takes the second and makes it all upper case,
+    create a third string which combines the two. Store the lowercase, uppercase and combined string in a dictionary]
 
+    Args:
+        string_1 ([str]): [random string]
+        string_2 ([str): [random string]
+
+    Returns:
+        [Dictionary]: [key = L: Storing upper case of string 1, key = U: storing string 2 lower case, key = C: for combined string of upper and lower case]
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
