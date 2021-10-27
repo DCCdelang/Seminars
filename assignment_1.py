2a6e3377160581d882d9381d0649e0da20db591e
Maurits Bos <Maurits.bos@ubiqu.eu>
2021-10-27 14:44:19 +0200

    Lolzz im the first Fuck you guys

--- assignment_1.py
+++ assignment_1.py
@@ -5,7 +5,7 @@ The fastest one to push is lucky, the others will most likely get a merge
 conflict. See if you can fix it :)
 """
 
-def lower_case(string):
+def lower_case(string: str):
     """
     Argument:
     string -- text you want to turn into lower case
@@ -16,7 +16,7 @@ def lower_case(string):
     """
 
     ### your code starts here
-
+    lower_string = string.lower()
     ### your code ends here
 
     return lower_string
@@ -34,7 +34,7 @@ def upper_case(string):
 
     Returns:
     upper_string -- upper case version of string
-    
+
     """
 
     ### your code starts here
