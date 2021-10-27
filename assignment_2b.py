4364bef3ead828bf75f795f9aef7e1176f49411f
Thomas Wierda <2621446@student.vu.nl>
2021-10-27 15:29:11 +0200

    added documentations

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,10 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    '''
+    takes in 2 strings. first string gets lower cased, second one uppercased.
+    return dictionary with first string lower cased under L, second string upper cased under U and combined under C.
+    '''
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
