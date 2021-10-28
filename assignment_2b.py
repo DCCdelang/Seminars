ff65fb1f4bbadd14b453fe30db4524813614ec0a
Senne Bakker <Senne.bakker@outlook.com>
2021-10-28 16:05:16 +0200

    Added documentation

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,6 +4,15 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+## DOCS ###
+
+# Function 2b can be used to modify strings. It takes two strings as input, and returns a dictionary with the lower case version
+# of the first input string, corresponding to the key L. Moreover, it returns the upper case version of the second input string,
+# corresponding to the dictionary key U and lastly it returns combined version of both strings.
+# So, it concatenates (the lower case) the first input string and the (upper case) second input string.
+
+
+
 def function_2b(string_1, string_2):
 
     lower = string_1.lower()
