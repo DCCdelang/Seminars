50102a50eb152a7036d67a8328b70ba0c202d22f
harshitachou <38243535+harshitachou@users.noreply.github.com>
2021-10-28 20:07:24 +0200

    Update assignment_2b.py
    adding documentation for the function_2b

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,6 +4,23 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+"""
+
+function_2b(string_1, string_2)
+Arguments - string_1 and string_2
+Return value - dictionary
+Key value pairs for the dictionary are as follows - 
+Key | value
+'L' | string_1 in lower case letters
+'U' | string_2 in upper case letters
+'C' | string_2 appended to string_1 (string_1 + string_2)
+
+"""
+
+
+
+
 def function_2b(string_1, string_2):
 
     lower = string_1.lower()
