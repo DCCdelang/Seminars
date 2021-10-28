ae4080164fb612da4499bc7f4ef1bc3166a1048c
Brent Muijskens <brent.muijskens@gmail.com>
2021-10-28 20:08:33 +0200

    all done :)

--- assignment_2b.py
+++ assignment_2b.py
@@ -1,11 +1,22 @@
-"""
-Write a documentation for the simple function below. Your partner will have to
-implement the function, without knowing the code. Send your partner the
-documentation and see if he can work with it.
-No cheating! Don't show or tell hem the code directly
-"""
 def function_2b(string_1, string_2):
+    """
+    Description
+    -----------
+    Function takes two strings and returns 3 variations of it in dictionary
 
+    Input
+    -----
+    string_1: str
+    string_2: str
+
+    Output
+    ------
+    dict: dicitonary
+            dictionary keys: 
+                "L": string_1 in lower case  
+                "U": stromg_2 in upper case 
+                "C": combination (additive) of string_1, string_2
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
