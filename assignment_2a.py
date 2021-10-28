08e3d5842698185ff26be0a2c483dd74e8822042
harshitachou <38243535+harshitachou@users.noreply.github.com>
2021-10-28 21:34:59 +0200

    Update assignment_2a.py
    Adding function description here

--- assignment_2a.py
+++ assignment_2a.py
@@ -9,6 +9,31 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
+
+"""
+
+function_2b(string_1, string_2)
+Arguments - string_1 and string_2
+Return value - dictionary
+Key value pairs for the dictionary are as follows - 
+Key | value
+'L' | string_1 in lower case letters
+'U' | string_2 in upper case letters
+'C' | string_2 appended to string_1 (string_1 + string_2)
+
+"""
+
+"""
+
+function_2c(w,x,y,z)
+Return value - dictionary with key value pairs
+key -> algebraic operations("multiply","divide","add","subtract")
+values -> x*y,x/y,w+z,w-z
+
+""""
+
+
+
 var_1 = function_2b(...)
 
 var_2 = function_2c(...)
